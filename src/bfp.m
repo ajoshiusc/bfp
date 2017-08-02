2ed2d9ce0e8bfd0abd0d8bf9ff3c310f7ca57d6f
Anand A Joshi <ajoshi@erdos.usc.edu>
2017-08-01 18:35:04 -0700

    update

--- src/bfp.m
+++ src/bfp.m
@@ -1,24 +1,34 @@
 %% BFP: BrainSuite fMRI Pipeline
+
 %% User Inputs
 
 t1 = '~/coding_ground/bfp/data/sub-01_ses-movie_task-movie_run-1_defacemask.nii.gz';
 fmri{1} = '~/coding_ground/bfp/data/sub-01_ses-movie_task-movie_run-1_bold.nii.gz';
 subbasename = '~/coding_ground/bfp/data/sub-01-run1/sub-01-run1';
+BrainSuitePath='/home/ajoshi/BrainSuite17a';
+RMFLAG=1;
 fprintf('Processing T1=%s\n',t1)
 for ind=1:length(fmri)
     fprintf('Processing fMRI=%s\n',fmri{ind})
 end
-%% 
-% 
+
 %% Create Directory Structure
+subdir=fileparts(subbasename);
+if RMFLAG && exist(subdir,'dir')
+    rmdir(subdir,'s')
+end
 
 mkdir(subbasename)
-mkdir(fullfile(subbasename,'anat'))
-
+anatDir=fullfile(subbasename,'anat');
+mkdir(anatDir);
+copyfile(t1,anatDir)
 for ind = 1:length(fmri)
-    mkdir(fullfile(subbasename,sprintf('func-%d',ind)))
+    outdir=fullfile(subbasename,sprintf('func-%d',ind));
+    mkdir(outdir);
+    copyfile(fmri{ind},outdir);
 end
-%% 
+
+%%
 %% Skull Strip MRI
 % 
 %% Coregister fMRI to MNI Space

ab9e93eb47fac39aa2ad0428ce31228c38f5fe72
Anand A Joshi <ajoshi@erdos.usc.edu>
2017-08-17 18:39:11 -0700

    update

--- src/Visualization/main_bfp_bipolar2gif.m
+++ src/Visualization/main_bfp_bipolar2gif.m
@@ -2,8 +2,8 @@ clear all;close all;clc;
 restoredefaultpath;
 
 bfpdir='/home/ajoshi/coding_ground/bfp';
-fmridatfile='/home/ajoshi/coding_ground/bfp/data/sub-01/func/sub-01_ses-movie_task-movie_run-1_bold.32k.GOrd.mat';
-outgiffile='fmri_Moviesub1.gif';
+fmridatfile='/home/ajoshi/coding_ground/bfp/data/sub-01/func/sub-01_ses-movie_task-movie_run-1_bold.32k.GOrd.filt.mat';
+outgiffile='fmri_Moviesub1_filt.gif';
 TR=2;
 
 addpath(genpath(fullfile(bfpdir,'src')));

c0283d043623c5896af47ef6d93468fa1f0e1ec9
Anand Joshi <ajoshi@hpc-login2-l.hpcc.usc.edu>
2017-09-01 17:59:37 -0700

    update

--- src/preproc/sample_main_qsub.m
+++ src/preproc/sample_main_qsub.m
@@ -7,17 +7,6 @@ configfile='/home/rcf-40/ajoshi/aaj/git_sandbox/bfp/supp_data/hpcconfig.ini';
 lst=dir('/home/rcf-40/ajoshi/aaj/Beijing_Zang/sub*');
 
 for jj=1:length(lst)
-    t1=['/home/rcf-40/ajoshi/aaj/Beijing_Zang/',lst(jj).name,'/anat/mprage_anonymized.nii.gz'];
-    fmri=['{''/home/rcf-40/ajoshi/aaj/Beijing_Zang/',lst(jj).name,'/func/rest.nii.gz''}'];
-    studydir='/home/rcf-40/ajoshi/aaj/git_sandbox/bfp/data/';
-    subid=lst(jj).name;
-    sessionid='{''rest''}';
-    TR='2';
-    
-    bfpcommand = sprintf('bfp %s %s %s %s %s %s %s', configfile, t1, fmri, studydir, subid, sessionid, TR);
-    cmd1=sprintf('/usr/usc/matlab/default/bin/matlab -nodisplay -nosplash -r "addpath(genpath(''/home/rcf-proj2/aaj/git_sandbox/svreg-matlab/src''));%s;exit;"',bfpcommand);
-    
-    fprintf(cmd1);
-    unix(['qsub ',cmd1]);
-    
+   unix(['qsub -v subid=',lst(jj).name,' /home/rcf-40/ajoshi/aaj/git_sandbox/bfp/supp_data/fcon1000_qsub.sh &']);
 end
+

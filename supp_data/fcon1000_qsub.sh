aa92c5b3eba5029d447af1ae1672ac174a6b8a89
Anand Joshi <ajoshi@hpc-login2-l.hpcc.usc.edu>
2017-09-01 20:47:33 -0700

    fcon1000 script updated

--- supp_data/fcon1000_qsub.sh
+++ supp_data/fcon1000_qsub.sh
@@ -4,7 +4,7 @@
 
 echo ${subid}
 
-/usr/usc/matlab/default/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/home/rcf-proj2/aaj/git_sandbox/bfp/src')); bfp /home/rcf-40/ajoshi/aaj/git_sandbox/bfp/supp_data/hpcconfig.ini /home/rcf-40/ajoshi/aaj/Beijing_Zang/${subid}/anat/mprage_anonymized.nii.gz {'/home/rcf-40/ajoshi/aaj/Beijing_Zang/${subid}/func/rest.nii.gz'} /home/rcf-40/ajoshi/aaj/git_sandbox/bfp/data/ ${subid} {'rest'} 2"
+/usr/usc/matlab/default/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/home/rcf-proj2/aaj/git_sandbox/bfp/src')); bfp /home/rcf-proj2/aaj/git_sandbox/bfp/supp_data/hpcconfig.ini /home/rcf-proj2/aaj/Beijing_Zang/${subid}/anat/mprage_anonymized.nii.gz {'/home/rcf-proj2/aaj/Beijing_Zang/${subid}/func/rest.nii.gz'} /home/rcf-proj2/aaj/git_sandbox/bfp/data/ ${subid} {'rest'} 2"
 
 exit 0
 

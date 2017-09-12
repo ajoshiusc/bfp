35d41aff257cb74250efde7f433149cfbb1cb3f7
ajoshi <ajoshi@erdos.usc.edu>
2017-09-11 18:04:43 -0700

    update

--- src/preproc/tNLMPDFGOrdfMRI.m
+++ src/preproc/tNLMPDFGOrdfMRI.m
@@ -2,7 +2,7 @@ function tNLMPDFGOrdfMRI(GOrdInFile,GOrdOutFile,config)
 
 load(GOrdInFile);
 option = tNLMPdf();
-option.FPR = config.fpr;
+option.FPR = str2double(config.fpr);
 option.memoryLimit = config.memory;
 option.SCBFile = config.scbPath;
 dtseries=tNLMPdf(dtseries, option);

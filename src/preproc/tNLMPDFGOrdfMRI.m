46fd9194aceae3cacb58429d2500202f485b28b5
Anand Joshi <ajoshi@hpc-login2-l.hpcc.usc.edu>
2017-09-15 14:09:48 -0700

    update

--- src/preproc/tNLMPDFGOrdfMRI.m
+++ src/preproc/tNLMPDFGOrdfMRI.m
@@ -3,6 +3,9 @@ function tNLMPDFGOrdfMRI(GOrdInFile,GOrdOutFile,config)
 load(GOrdInFile);
 option = tNLMPdf();
 option.FPR = str2double(config.fpr);
+if ~strcmpi(config.memory,'auto')
+    config.memory=str2double(config.memory);
+end
 option.memoryLimit = config.memory;
 option.SCBFile = config.scbPath;
 dtseries=tNLMPdf(dtseries, option);
--- supp_data/fcon1000_qsub.sh
+++ supp_data/fcon1000_qsub.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=4
-#PBS -l walltime=11:50:00
-
+#PBS -l walltime=00:59:59
+#PBS -l mem=32gb
+#PBS -l vmem=32gb
+#PBS -m a
 echo ${subid}
 
 /usr/usc/matlab/default/bin/matlab -nodisplay -nosplash -r "addpath(genpath('/home/rcf-proj2/aaj/git_sandbox/bfp/src')); bfp /home/rcf-proj2/aaj/git_sandbox/bfp/supp_data/hpcconfig.ini /home/rcf-proj2/aaj/Beijing_Zang/${subid}/anat/mprage_anonymized.nii.gz {'/home/rcf-proj2/aaj/Beijing_Zang/${subid}/func/rest.nii.gz'} /home/rcf-proj2/aaj/git_sandbox/bfp/data/ ${subid} {'rest'} 2"

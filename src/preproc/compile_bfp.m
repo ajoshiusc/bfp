49b3d401a2c43ed27c42319b0d5484c91498a0b6
Anand A Joshi <ajoshi@masal.usc.edu>
2017-10-10 23:26:49 -0700

    update

--- src/preproc/compile_bfp.m
+++ src/preproc/compile_bfp.m
@@ -2,20 +2,23 @@ function compile_bfp(bfp_version)
 restoredefaultpath;
 addpath(genpath('../../src'));
 
+bfp_bin_dir=sprintf('bfp_%s', bfp_version);
 fid = fopen('bfp_version.txt', 'w');
 fprintf(fid, bfp_version);
 fclose(fid);
 
-if exist(sprintf('bfp_%s', bfp_version),'dir')
-    rmdir(sprintf('bfp_%s', bfp_version));
+if exist(bfp_bin_dir,'dir')
+    rmdir(bfp_bin_dir,'s');
 end
 
-mkdir(sprintf('bfp_%s', bfp_version))
+mkdir(bfp_bin_dir);
 mcc -m -v bfp.m
-copyfile('../scripts/bfp_linux.sh', sprintf('./bfp_%s/bfp.sh', bfp_version));
-movefile('bfp', sprintf('bfp_%s', bfp_version));
-copyfile('../../supp_data', sprintf('bfp_%s/supp_data', bfp_version));
-copyfile('bfp_version.txt',sprintf('bfp_%s', bfp_version));
+copyfile('../scripts/bfp_linux.sh', [bfp_bin_dir,'/bfp.sh']);
+movefile('bfp', bfp_bin_dir);
+copyfile('../../supp_data', [bfp_bin_dir,'/supp_data']);
+copyfile('bfp_version.txt',bfp_bin_dir);
 
+tar(sprintf('bfp_%s.tar.gz', bfp_version),bfp_bin_dir);
+rmdir(bfp_bin_dir, 's');
 
 

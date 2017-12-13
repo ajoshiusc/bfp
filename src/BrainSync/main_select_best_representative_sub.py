af317becfd0ade2410bc58d8044e0b427165bbd6
Anand A Joshi <ajoshi@masal.usc.edu>
2017-12-13 02:16:47 -0800

    update

--- src/BrainSync/main_select_best_representative_sub.py
+++ src/BrainSync/main_select_best_representative_sub.py
@@ -6,43 +6,30 @@ from fmri_methods_sipi import rot_sub_data
 from surfproc import view_patch_vtk, patch_color_attrib
 from dfsio import readdfs
 import os
+from brainsync import normalizeData, brainSync
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
+import h5py
 
-p_dir = '/big_disk/ajoshi/HCP_data/data'
-p_dir_ref = '/big_disk/ajoshi/HCP_data'
+p_dir = '/big_disk/ajoshi/HCP_Data_For_Haleh'
 lst = os.listdir(p_dir)
 
-r_factor = 3
-ref_dir = os.path.join(p_dir_ref, 'reference')
-nClusters = 30
-
-ref = '196750'#'100307'
-print(ref + '.reduce' + str(r_factor) + '.LR_mask.mat')
-fn1 = ref + '.reduce' + str(r_factor) + '.LR_mask.mat'
-fname1 = os.path.join(ref_dir, fn1)
-msk = scipy.io.loadmat(fname1)  # h5py.File(fname1);
-dfs_right = readdfs(os.path.join(p_dir_ref, 'reference', ref + '.aparc.\
-a2009s.32k_fs.reduce3.right.dfs'))
-dfs_right_sm = readdfs(os.path.join(p_dir_ref, 'reference', ref + '.aparc.\
-a2009s.32k_fs.reduce3.very_smooth.right.dfs'))
-count1 = 0
-rho_rho = []
-rho_all = []
-labs_all = sp.zeros((len(dfs_right.labels), len(lst)))
+fn1 = 'rfMRI_1_LR.mat'
+fname1 = os.path.join(p_dir, fn1)
+mskf= h5py.File(fname1, 'r') 
+print(list(mskf.keys()))
+mskf.close()
 
 for sub in lst:
-    data = scipy.io.loadmat(os.path.join(p_dir, sub, sub + '.rfMRI_REST1_LR.\
-reduce3.ftdata.NLM_11N_hvar_25.mat'))
-    LR_flag = msk['LR_flag']
-    LR_flag = np.squeeze(LR_flag) == 0
-    data = data['ftdata_NLM']
-    temp = data[LR_flag, :]
-    m = np.mean(temp, 1)
-    temp = temp - m[:, None]
-    s = np.std(temp, 1)+1e-16
-    temp = temp/s[:, None]
-    d = temp
+    fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
+    df = h5py.File(fname, 'r')
+    
+    dataL = df['dataL']
+    dataR = df['dataR']
+    
+    data = sp.concatenate((dataL,dataR),axis=1)
+    
+    
     if count1 == 0:
         sub_data = sp.zeros((d.shape[0], d.shape[1], len(lst)))
 

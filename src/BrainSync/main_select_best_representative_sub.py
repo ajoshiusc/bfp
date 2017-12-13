2df6c5013797d35a311d5c406279cdfd4f0115ee
Anand A Joshi <ajoshi@masal.usc.edu>
2017-12-13 12:09:12 -0800

    update

--- src/BrainSync/main_select_best_representative_sub.py
+++ src/BrainSync/main_select_best_representative_sub.py
@@ -13,59 +13,62 @@ import h5py
 
 p_dir = '/big_disk/ajoshi/HCP_Data_For_Haleh'
 lst = os.listdir(p_dir)
-
+count1 = 0
 fn1 = 'rfMRI_1_LR.mat'
 fname1 = os.path.join(p_dir, fn1)
-mskf= h5py.File(fname1, 'r') 
+mskf = h5py.File(fname1, 'r')
 print(list(mskf.keys()))
 mskf.close()
+nsub = 0
+for sub in lst:
+    fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
+    if not os.path.isfile(fname):
+        continue
+    nsub += 1
 
 for sub in lst:
     fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
+    if not os.path.isfile(fname):
+        continue
     df = h5py.File(fname, 'r')
-    
     dataL = df['dataL']
     dataR = df['dataR']
-    
-    data = sp.concatenate((dataL,dataR),axis=1)
-    
-    
+    d = sp.concatenate((dataL, dataR), axis=1)
+    d, _, _ = normalizeData(d)
     if count1 == 0:
-        sub_data = sp.zeros((d.shape[0], d.shape[1], len(lst)))
+        sub_data = sp.zeros((d.shape[0], d.shape[1], nsub))
 
     sub_data[:, :, count1] = d
     count1 += 1
     print count1,
 
+
 nSub = sub_data.shape[2]
-rperm = sp.random.permutation(dfs_right_sm.vertices.shape[0])
-#rperm=range(dfs_right_sm.vertices.shape[0])
 dist_all_orig = sp.zeros([nSub, nSub])
 dist_all_rot = dist_all_orig.copy()
-#sub_data[:,:,1]=sub_data[rperm,:,1]
 sub_data_orig = sub_data.copy()
 
 for ind1 in range(nSub):
     for ind2 in range(nSub):
         dist_all_orig[ind1, ind2] = sp.linalg.norm(sub_data_orig[:, :, ind1] -
                                                    sub_data_orig[:, :, ind2])
-        sub_data_rot, _ = rot_sub_data(ref=sub_data[:, :, ind1],
-                                       sub=sub_data[:, :, ind2])
+        sub_data_rot, _ = brainSync(X=sub_data[:, :, ind1],
+                                    Y=sub_data[:, :, ind2])
         dist_all_rot[ind1, ind2] = sp.linalg.norm(sub_data[:, :, ind1] -
                                                   sub_data_rot)
         print ind1, ind2
 
 
-sp.savez('rot_pairwise_dist_all_sub_by_sub.npz', dist_all_rot=dist_all_rot,
+sp.savez('Haleh_pairwise_dist_all_sub_by_sub.npz', dist_all_rot=dist_all_rot,
          dist_all_orig=dist_all_orig, lst=lst)
 ######
 
 a = sp.load('rot_pairwise_dist_all_sub_by_sub.npz')
 q = sp.argmin(a['dist_all_rot'].sum(1))
-m=MDS(n_components=3,dissimilarity='precomputed')
-e=m.fit_transform(a['dist_all_rot'])
+m = MDS(n_components=3, dissimilarity='precomputed')
+e = m.fit_transform(a['dist_all_rot'])
 print(e)
 fig, ax = plt.subplots()
-ax.scatter(e[:,0],e[:,1])
+ax.scatter(e[:, 0], e[:, 1])
 for i in range(e.shape[0]):
-    ax.annotate(lst[i], (e[i,0],e[i,1]))
+    ax.annotate(lst[i], (e[i, 0], e[i, 1]))

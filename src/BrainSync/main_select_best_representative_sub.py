747d598fa8a1e63764fc6b3127d0964f04770605
Anand A Joshi <ajoshi@masal.usc.edu>
2017-12-19 15:18:04 -0800

    git push

--- src/BrainSync/main_select_best_representative_sub.py
+++ src/BrainSync/main_select_best_representative_sub.py
@@ -10,7 +10,7 @@ from brainsync import normalizeData, brainSync
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
 import h5py
-
+#%%
 p_dir = '/big_disk/ajoshi/HCP_Data_For_Haleh'
 lst = os.listdir(p_dir)
 count1 = 0
@@ -42,16 +42,21 @@ for sub in lst:
     count1 += 1
     print count1,
 
+#%%
+r = h5py.File('/big_disk/ajoshi/fmri_Atlas_Result/result_raw.mat')
+atlas = sp.array(r['X2']).T
+sub_data = sp.concatenate((sub_data, atlas[:, :, None]), axis=2)
 
 nSub = sub_data.shape[2]
+print nSub
 dist_all_orig = sp.zeros([nSub, nSub])
 dist_all_rot = dist_all_orig.copy()
-sub_data_orig = sub_data.copy()
+#sub_data_orig = sub_data.copy()
 
 for ind1 in range(nSub):
     for ind2 in range(nSub):
-        dist_all_orig[ind1, ind2] = sp.linalg.norm(sub_data_orig[:, :, ind1] -
-                                                   sub_data_orig[:, :, ind2])
+        dist_all_orig[ind1, ind2] = sp.linalg.norm(sub_data[:, :, ind1] -
+                                                   sub_data[:, :, ind2])
         sub_data_rot, _ = brainSync(X=sub_data[:, :, ind1],
                                     Y=sub_data[:, :, ind2])
         dist_all_rot[ind1, ind2] = sp.linalg.norm(sub_data[:, :, ind1] -
@@ -59,13 +64,13 @@ for ind1 in range(nSub):
         print ind1, ind2
 
 
-sp.savez('Haleh_pairwise_dist_all_sub_by_sub.npz', dist_all_rot=dist_all_rot,
+sp.savez('Haleh_pairwise_dist_all_sub_by_sub2.npz', dist_all_rot=dist_all_rot,
          dist_all_orig=dist_all_orig, lst=lst)
 ######
 #%%
-a = sp.load('Haleh_pairwise_dist_all_sub_by_sub.npz')
+a = sp.load('Haleh_pairwise_dist_all_sub_by_sub2.npz')
 lst = a['lst']
-lst2 = [None]*nsub
+lst2 = [None]*(nsub+1)
 ctr = 0
 for sub in lst:
     fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
@@ -73,7 +78,7 @@ for sub in lst:
         continue
     lst2[ctr] = sub
     ctr += 1
-
+lst2[nsub] = 'JSGA atlas'
 
 q = sp.argmin(a['dist_all_rot'].sum(1))
 m = MDS(n_components=2, dissimilarity='precomputed')
@@ -84,7 +89,7 @@ ax.scatter(e[:, 0], e[:, 1])
 for i in range(e.shape[0]):
     ax.annotate(lst2[i], (e[i, 0], e[i, 1]))
 
-## Compute difference
+#%% Compute difference
 diff = 0
 for ind in range(nsub):
     Y2, _ = brainSync(X=sub_data[:, :, q], Y=sub_data[:, :, ind])

846085abed7028084a7b93aa2136cbb4240f503e
ajoshi <ajoshi@hpc-login2-l.hpcc.usc.edu>
2018-06-22 00:36:47 -0700

    update

--- src/preproc/sample_main_qsub.m
+++ src/preproc/sample_main_qsub.m
@@ -4,9 +4,11 @@ addpath(genpath('/home/rcf-proj2/aaj/git_sandbox/bfp'));
 configfile='/home/rcf-proj2/aaj/git_sandbox/bfp/supp_data/hpcconfig.ini';
 %T1 and fMRI images in NIFTI-1 format (nii.gz)
 
-lst=dir('/home/rcf-proj2/aaj/Beijing_Zang/sub*');
+lst=dir('/home/rcf-proj2/aaj/ADHD_Peking/data/Peking_all/*');
 
-for jj=1:length(lst)
-   unix(['qsub -v subid=',lst(jj).name,' /home/rcf-proj2/aaj/git_sandbox/bfp/supp_data/fcon1000_qsub.sh']);
+for jj=length(lst):-1:1
+    if ~exist(['/home/rcf-proj2/aaj/ADHD_Peking_bfp/',lst(jj).name,'/func/',lst(jj).name,'_rest_bold.32k.GOrd.mat'],'file')
+        unix(['sbatch --export=subid=',lst(jj).name,' /home/rcf-proj2/aaj/git_sandbox/bfp/supp_data/fcon1000_slurm.sh']);
+    end
 end
 

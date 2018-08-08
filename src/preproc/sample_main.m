7cc579aa013f43fb3e0a01e487abb666251d3682
Anand A Joshi <ajoshi@erdos.usc.edu>
2018-08-08 00:43:03 -0700

    update

--- src/preproc/sample_main.m
+++ src/preproc/sample_main.m
@@ -2,11 +2,11 @@ clc;clear all;close all;restoredefaultpath;
 addpath(genpath('/home/ajoshi/coding_ground/bfp/src'));
 
 % Set the input arguments
-configfile='/home/ajoshi/bfp_sample/bfpconfig.ini';
-t1='/home/ajoshi/bfp_sample/input/sub08251/anat/mprage_anonymized.nii.gz';
-fmri='/home/ajoshi/bfp_sample/input/sub08251/func/rest.nii.gz';
-studydir='/home/ajoshi/bfp_sample/output';
-subid='sub08251';
+configfile='/home/ajoshi/coding_ground/bfp/supp_data/config.ini';
+t1='/home/ajoshi/Desktop/for_SY/SCD062.nii.gz';
+fmri='/home/ajoshi/Desktop/for_SY/SCD062.BOLD.resting.nii.gz';
+studydir='/home/ajoshi/Desktop/for_SY/bfpout';
+subid='SCD062';
 sessionid='rest';
 TR='2';
  

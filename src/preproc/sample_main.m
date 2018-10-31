f5412faea29a63bb7f7cb271f023a8649dd4677c
Anand A Joshi <ajoshi@masal.usc.edu>
2018-10-31 13:40:29 -0700

    update

--- src/preproc/sample_main.m
+++ src/preproc/sample_main.m
@@ -1,15 +1,16 @@
 clc;clear all;close all;restoredefaultpath;
-addpath(genpath('/home/ajoshi/coding_ground/bfp/src'));
+addpath(genpath('/big_disk/ajoshi/coding_ground/bfp/src'));
 
 % Set the input arguments
-configfile='/home/ajoshi/coding_ground/bfp/supp_data/config.ini';
-t1='/home/ajoshi/BFP_issues/ACTL030/ACTL030.nii.gz';
-fmri='/home/ajoshi/BFP_issues/ACTL030/ACTL030.BOLD.resting.correct.nii.gz';
-studydir='/home/ajoshi/Desktop/for_SY/bfpout';
-subid='ACTL030';
+configfile='/big_disk/ajoshi/bfp_sample/config.ini';
+t1='/big_disk/ajoshi/bfp_sample/input/sub08001/anat/mprage_anonymized.nii.gz';
+fmri='/big_disk/ajoshi/bfp_sample/input/sub08001/func/rest.nii.gz';
+studydir='/big_disk/ajoshi/bfp_sample/output/matlab';
+subid='sub11';
 sessionid='rest';
 TR='2';
  
 % Call the bfp function
 bfp(configfile, t1, fmri, studydir, subid, sessionid, TR);
 
+%bfp.sh config.ini input/sub08001/anat/mprage_anonymized.nii.gz /big_disk/ajoshi/bfp_sample/input/sub08001/func/rest.nii.gz /big_disk/ajoshi/bfp_sample/output sub11 rest 2

c35faf586b07410d29f722cf14330c255f6e4a99
Anand A Joshi <ajoshi@masal.usc.edu>
2018-11-09 20:46:12 -0800

    update

--- src/BrainSync/main_select_best_representative_sub.py
+++ src/BrainSync/main_select_best_representative_sub.py
@@ -6,50 +6,41 @@ from fmri_methods_sipi import rot_sub_data
 from surfproc import view_patch_vtk, patch_color_attrib
 from dfsio import readdfs
 import os
+import glob
 from brainsync import normalizeData, brainSync
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
 import h5py
 #%%
-p_dir = '/big_disk/ajoshi/HCP_Data_For_Haleh'
-lst = os.listdir(p_dir)
+# This is the name of the directory that contains the grayordinate files
+p_dir = '/ImagePTE1/ajoshi/BFP_samples'
+lst = glob.glob(p_dir + '/*.mat')
 count1 = 0
-spio.savemat('test1.mat', {'count1': count1})
 
-fn1 = 'rfMRI_1_LR.mat'
-fname1 = os.path.join(p_dir, fn1)
-mskf = h5py.File(fname1, 'r')
-print(list(mskf.keys()))
-mskf.close()
-nsub = 0
-for sub in lst:
-    fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
+# Get number of subjects
+nsub = len(lst)
+lst2 = list([])
+
+for fname in lst:
     if not os.path.isfile(fname):
         continue
-    nsub += 1
-
-for sub in lst:
-    fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
-    if not os.path.isfile(fname):
-        continue
-    df = h5py.File(fname, 'r')
-    dataL = df['dataL']
-    dataR = df['dataR']
-    d = sp.concatenate((dataL, dataR), axis=1)
+    df = spio.loadmat(fname)  #, 'r')
+    d = df['dtseries']
+    #   dataR = df['dataR']
+    #   d = sp.concatenate((dataL, dataR), axis=1)
     d, _, _ = normalizeData(d)
     if count1 == 0:
         sub_data = sp.zeros((d.shape[0], d.shape[1], nsub))
 
     sub_data[:, :, count1] = d
+    lst2.append(fname)
     count1 += 1
     print(count1, )
 
-#%%
-r = h5py.File('/big_disk/ajoshi/fmri_Atlas_Result/result_raw.mat')
-atlas = sp.array(r['X2']).T
-sub_data = sp.concatenate((sub_data, atlas[:, :, None]), axis=2)
+#%% Compute pairwise distance
+nSub = count1
+sub_data = sub_data[:, :, :nSub]
 
-nSub = sub_data.shape[2]
 print(nSub)
 dist_all_orig = sp.zeros([nSub, nSub])
 dist_all_rot = dist_all_orig.copy()
@@ -60,68 +51,21 @@ for ind1 in range(nSub):
         dist_all_orig[ind1, ind2] = sp.linalg.norm(sub_data[:, :, ind1] -
                                                    sub_data[:, :, ind2])
         sub_data_rot, _ = brainSync(
-            X=sub_data[:, :, ind1], Y=sub_data[:, :, ind2])
+            X=sub_data[:, :, ind1].T, Y=sub_data[:, :, ind2].T)
         dist_all_rot[ind1, ind2] = sp.linalg.norm(sub_data[:, :, ind1] -
-                                                  sub_data_rot)
-        print(ind1, ind2)
+                                                  sub_data_rot.T)
+        print(ind1, ind2, dist_all_rot[ind1, ind2])
 
-sp.savez(
-    'Haleh_pairwise_dist_all_sub_by_sub2.npz',
-    dist_all_rot=dist_all_rot,
-    dist_all_orig=dist_all_orig,
-    lst=lst)
-######
-#%%
-a = sp.load('Haleh_pairwise_dist_all_sub_by_sub2.npz')
-lst = a['lst']
-lst2 = [None] * (nsub + 1)
-ctr = 0
-for sub in lst:
-    fname = os.path.join(p_dir, sub, 'rfMRI_1_LR.mat')
-    if not os.path.isfile(fname):
-        continue
-    lst2[ctr] = sub
-    ctr += 1
-lst2[nsub] = 'JSGA atlas'
-
-q = sp.argmin(a['dist_all_rot'][:-1, :-1].sum(1))
+q = sp.argmin(dist_all_rot.sum(1))
 print('The representative subject is: %s ' % lst2[q])
 m = MDS(n_components=2, dissimilarity='precomputed')
-e = m.fit_transform(a['dist_all_rot'])
+e = m.fit_transform(dist_all_rot)
 print(e)
 fig, ax = plt.subplots()
 ax.scatter(e[:, 0], e[:, 1])
 for i in range(e.shape[0]):
-    ax.annotate(lst2[i], (e[i, 0], e[i, 1]))
+    ax.annotate(lst2[i][30:37], (e[i, 0], e[i, 1]))
 
-#%% Compute difference
-diff = 0
-for ind in range(nsub):
-    Y2, _ = brainSync(X=sub_data[:, :, q], Y=sub_data[:, :, ind])
-    diff += (Y2 - sub_data[:, :, q])**2
-
-spio.savemat('diff_individual_atlas.mat', {'diff': diff})
-
-#%% Create Average atlas by synchronizing everyones data to one subject
-atlas = 0
-for ind in range(nsub):
-    Y2, _ = brainSync(X=sub_data[:, :, q], Y=sub_data[:, :, ind])
-    atlas += Y2
-atlas /= nsub
-
-diff = 0
-for ind in range(nsub):
-    Y2, _ = brainSync(X=atlas, Y=sub_data[:, :, ind])
-    diff += (Y2 - atlas)**2
-    print(ind, )
-
-spio.savemat('diff_average_atlas.mat', {'diff': diff})
-
-#%% Compute difference for the virtual subject
-diff = 0
-r = h5py.File('/big_disk/ajoshi/fmri_Atlas_Result/result_raw.mat')
-atlas = sp.array(r['X2']).T
-for ind in range(nsub):
-    Y2, _ = brainSync(X=atlas, Y=sub_data[:, :, ind])
-    diff += (Y2 - atlas)**2
-spio.savemat('diff_avg_atlas.mat', {'diff': diff})
+ax.set_title('MDS Plot of the subjects')
+plt.savefig('MDS_plot_of_subjects.pdf')
+print('Done')

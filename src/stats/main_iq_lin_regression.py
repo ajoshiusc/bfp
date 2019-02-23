1c4cea11fd475be53792838754e3b4305f8a2196
Anand A Joshi <ajoshi@erdos.usc.edu>
2019-02-22 22:21:35 -0800

    update

--- src/stats/main_iq_lin_regression.py
+++ src/stats/main_iq_lin_regression.py
@@ -62,7 +62,7 @@ def main():
     sub_ids, reg_var, sub_files = read_fcon1000_data(
         csv_fname=CSV_FILE,
         data_dir=DATA_DIR,
-        reg_var_name='ADHD Index',  #'Verbal IQ', 
+        reg_var_name='ADHD Index', #'Verbal IQ',  #'ADHD Index',  # 
         num_sub=NUM_SUB_ATLAS)
 
     # ### Generate average subject
@@ -80,11 +80,11 @@ def main():
     sub_ids, reg_var, sub_files = read_fcon1000_data(
         csv_fname=CSV_FILE,
         data_dir=DATA_DIR,
-        reg_var_name='ADHD Index',
+        reg_var_name='ADHD Index',#'Verbal IQ',  #
         num_sub=NUM_SUB)
 
     # Shuffle reg_var for testing
-    #reg_var = sp.random.permutation(reg_var)
+    reg_var = sp.random.permutation(reg_var)
 
     print('performing stats based on distance to atlas')
     corr_pval, corr_pval_fdr = dist2atlas_reg(
@@ -99,7 +99,7 @@ def main():
     vis_save_pval(
         bfp_path=BFPPATH, pval_map=corr_pval_fdr, surf_name='dist_corr_fdr')
 
-    for ndim in range(5, 145, 5):
+    for ndim in range(20, 21, 1):
         print('performing stats based on linear regression')
         lin_pval, lin_pval_fdr = lin_reg(
             bfp_path=BFPPATH,

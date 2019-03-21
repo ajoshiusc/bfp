9d324859737510c6fca4eb82a8dd56343bc30971
Anand A Joshi <ajoshi@erdos.usc.edu>
2019-03-20 22:56:45 -0700

    update

--- src/stats/main_lin_regression.py
+++ src/stats/main_lin_regression.py
@@ -30,7 +30,7 @@ CSV_FILE = '/deneb_disk/ADHD_Peking_bfp/Peking_all_phenotypic.csv'
 # 3. ADHD-inattentive.
 
 LEN_TIME = 235  # length of the time series
-NUM_SUB = 10  # Number of subjects for the study
+NUM_SUB = 150  # Number of subjects for the study
 
 
 def main():

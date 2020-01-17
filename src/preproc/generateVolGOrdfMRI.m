d48b2904a66b3ddcc0e87e681b88258e10167004
sychoi <choisoyo@gmail.com>
2020-01-16 22:52:01 -0800

    fixed bug
    Should not affect anything if of 3rd dimension is smaller than 4th dimension before bug fix. 
    Else, should have thrown error as size of 4th dimension (time) does not match between surface and volume.

--- src/preproc/generateVolGOrdfMRI.m
+++ src/preproc/generateVolGOrdfMRI.m
@@ -18,7 +18,7 @@ gr_x=invmapx(bci_vol_ind(ind));gr_x=gr_x*t1res(1)/fres(1);
 gr_y=invmapy(bci_vol_ind(ind));gr_y=gr_y*t1res(2)/fres(2);
 gr_z=invmapz(bci_vol_ind(ind));gr_z=gr_z*t1res(3)/fres(3);
 
-Vol_GO_fmri=zeros(length(bci_vol_ind),size(f.img,3));
+Vol_GO_fmri=zeros(length(bci_vol_ind),size(f.img,4));
 for t=1:size(f.img,4)
     Vol_GO_fmri(ind,t)=interp3(f.img(:,:,:,t),gr_y,gr_x,gr_z);
 end

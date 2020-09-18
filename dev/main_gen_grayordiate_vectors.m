d1ff09abc63c2fdfa5eeced9770a165617d408b1
Anand A Joshi <ajoshi@masal.usc.edu>
2020-09-17 18:47:14 -0700

    update to USCBrain labels

--- dev/main_gen_grayordiate_vectors.m
+++ dev/main_gen_grayordiate_vectors.m
@@ -2,18 +2,18 @@
 % The purpose of this file is to generate a file that gives a vector of
 % grayordinate labels, its id lookup tables
 clc;clear all;close all;
-addpath(genpath('/home/ajoshi/coding_ground/bfp/dev/gifti-1.6'));
-addpath(genpath('/home/ajoshi/coding_ground/svreg/src'));
-addpath(genpath('/home/ajoshi/coding_ground/svreg/3rdParty'));
+addpath(genpath('/ImagePTE1/ajoshi/code_farm/bfp/dev/gifti-1.6'));
+addpath(genpath('/ImagePTE1/ajoshi/code_farm/svreg/src'));
+addpath(genpath('/ImagePTE1/ajoshi/code_farm/svreg/3rdParty'));
 
 % Read surface indices
 load('../supp_data/bci_grayordinates_surf_ind.mat')
 load('../supp_data/HCP_32k_Label.mat')
 hcp_isnan=isnan(brainstructure(1:2*32492));
 
-l= readdfs('/home/ajoshi/coding_ground/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.left.mid.cortex.dfs');
+l= readdfs('/ImagePTE1/ajoshi/code_farm/svreg/USCBrain/USCBrain.left.mid.cortex.dfs');
 
-r= readdfs('/home/ajoshi/coding_ground/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.right.mid.cortex.dfs');
+r= readdfs('/ImagePTE1/ajoshi/code_farm/svreg/USCBrain/USCBrain.right.mid.cortex.dfs');
 
 surf_labs=[l.labels(ind_left);r.labels(ind_right)];
 
@@ -21,9 +21,9 @@ surf_labs(hcp_isnan)=0;
 
 % Read indices for volume and read labels from the corresponding volume
 
-load('/home/ajoshi/coding_ground/bfp/supp_data/bci_grayordinates_vol_ind.mat')
+load('../supp_data/bci_grayordinates_vol_ind.mat')
 bci_vol_ind(isnan(bci_vol_ind))=1;
-vl = load_nii('/home/ajoshi/coding_ground/svreg/BCI-DNI_brain_atlas/BCI-DNI_brain.label.nii.gz');
+vl = load_nii('/ImagePTE1/ajoshi/code_farm/svreg/USCBrain/USCBrain.label.nii.gz');
 
 vol_labs=vl.img(bci_vol_ind);
 
@@ -31,7 +31,7 @@ vol_labs=vl.img(bci_vol_ind);
 labels=[surf_labs;vol_labs(1+length(surf_labs):end)];
 
 % Read XML and create lookup table
-xml1 = xml2struct('/home/ajoshi/coding_ground/svreg/BCI-DNI_brain_atlas/brainsuite_labeldescription.xml');
+xml1 = xml2struct('/ImagePTE1/ajoshi/code_farm/svreg/USCBrain/brainsuite_labeldescription.xml');
 
 num_rois = length(xml1.labelset.label);
 Ids = zeros(num_rois,1);
@@ -46,7 +46,7 @@ end
 LookUpTable.IDs=IDs;
 LookUpTable.IDNames=IDNames;
 
-save('BCI-DNI_brain_grayordinate_labels.mat', 'LookUpTable', 'labels');
+save('USCBrain_grayordinate_labels.mat', 'LookUpTable', 'labels');
 
 
 

0220bcc7992a202eb8c20adcca19f8bde5d5595d
Anand A Joshi <ajoshi@masal.usc.edu>
2020-12-01 18:01:39 -0800

    update

--- dev/main_plot_spheres.m
+++ dev/main_plot_spheres.m
@@ -1,7 +1,7 @@
 clear all;close all;clc;
 restoredefaultpath;
 addpath('/ImagePTE1/ajoshi/code_farm/svreg/src');
-
+addpath('/ImagePTE1/ajoshi/code_farm/svreg/3rdParty/')
 figure;
 mysphere([0,0,0],100,[.5,.5,.5],100);
 hold on;axis equal;material dull;camlight;
@@ -35,6 +35,29 @@ for j=1:length(x)
 end
 camlight;
 
+%%
+r=readdfs('/ImagePTE1/ajoshi/code_farm/bfp/supp_data/bci32kright.dfs');
+l=readdfs('/ImagePTE1/ajoshi/code_farm/bfp/supp_data/bci32kleft.dfs');
 
+c=jet(10);
+for j=1:10
+    f=view_patch(r);
+    view(90,0);
+    %mysphere(cursor_info1.Position,5,'r',100)
+    mysphere([109.2022   48.3260  156.7884],4,c(j,:),100);
+    material dull;
+    axis tight;
+    saveas(f,sprintf('brain_%d.png',j));
+    close all;    
+end
 
+%%
+load('/ImagePTE1/ajoshi/code_farm/bfp/supp_data/USCLobes_grayordinate_labels.mat');
+r=readdfs('/ImagePTE1/ajoshi/code_farm/bfp/supp_data/bci32kright.dfs');
+c=0.5+0*r.vertices;
+c(labels(32492+1:2*32492)==200,1:2)=1;
+patch('vertices',r.vertices,'faces',r.faces,'facevertexcdata',c,'edgecolor','none','facecolor','flat');
+view(90,0);axis off;axis tight;axis equal;
+camlight;
+material dull;
 

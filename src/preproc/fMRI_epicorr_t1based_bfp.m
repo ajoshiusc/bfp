c19a1ba14ef7ca70afcc50ecffbba10896de16e1
sychoi <choisoyo@gmail.com>
2021-01-16 16:31:54 -0800

    removed unnecessary modules

--- src/preproc/fMRI_epicorr_t1based_bfp.m
+++ src/preproc/fMRI_epicorr_t1based_bfp.m
@@ -23,46 +23,6 @@ t = now;
 d = datetime(t,'ConvertFrom','datenum');
 fprintf(fp,'\n%s\n',d);
 fprintf(fp, 'Running T1-based distortion correction \n');
-%%  reorient reference/example file
-% refRAS = [fmri_b0,'.ref.RAS.nii.gz'];
-% if ~exist(refRAS,'file')
-%     if exist(ref_filename,'file')
-%         reorient_nifti_sform(ref_filename,refRAS);
-%         msg = ['reference file reoriented to RAS format: ', refRAS];
-%         disp(msg);
-%         fprintf(fp, '%s\n',msg);
-%     else
-%         msg = ['error! reference file not found: ', ref_filename];
-%         disp(msg)
-%         fprintf(fp, '%s\n', msg);
-%         error(msg);
-% 
-%     end
-% else
-%     msg = ['reference file already reoriented'];
-%     disp(msg)
-%     fprintf(fp, '%s\n', msg);
-% end
-%%  reorient mc/epi file to RAS format
-% epiRAS = [remove_extension(epi_filename),'.RAS.nii.gz'];
-% if ~exist(epiRAS,'file')
-%     if exist(epi_filename,'file')
-%         reorient_nifti_sform(epi_filename,epiRAS);
-%         msg = ['input epi file reoriented to RAS format: ', epiRAS];
-%         disp(msg);
-%         fprintf(fp, '%s\n',msg);
-%     else
-%         msg = ['error! input epi file not found: ', epi_filename];
-%         disp(msg)
-%         fprintf(fp, '%s\n', msg);
-%         error(msg);
-% 
-%     end
-% else
-%     msg = ['input epi file already reoriented'];
-%     disp(msg)
-%     fprintf(fp, '%s\n', msg);
-% end
 %%  create temp volume with ref as 1st volume
 infile = [fmri_b0,'.infile.nii.gz'];
 
@@ -95,8 +55,6 @@ infileRAS = [fmri_b0,'.infile.RAS.nii.gz'];
 if ~exist(infileRAS,'file')
     reorient_nifti_sform(infile,infileRAS);
 end
-% t1RAS = [fmri_b0,'.t1.bfc.RAS.nii.gz'];
-% reorient_nifti_sform(struct_filename,t1RAS);
 %%  mask epi
 epimask_filename = [fmri_b0,'.mask.nii.gz'];
 if config.epit1corr_mask == '0'
@@ -117,27 +75,7 @@ elseif config.epit1corr_mask=='3'
     fprintf(fp, '--Masking method: AFNI 3dAutomask dilate 2 \n');
     unix(['3dAutomask -prefix ', epimask_filename,' -dilate 2 ',ref_filename]);
 end
-reorient_nifti_sform(epimask_filename,epimask_filename);
-%%  initial rigid registration
-output_filename = [fmri_b0,'.ref2t1.nii.gz'];
-regmat = [remove_extension(output_filename),'.rigid_registration_result.mat'];
-if ~exist(regmat,'file')
-    msg = 'Running initial rigid registration';
-    disp(msg);
-    fprintf(fp, '%s\n',msg);
-    msg = 'Running initial rigid registration using INVERSION';
-    disp(msg);
-    fprintf(fp, '%s\n',msg);
-    moving_filename = ref_filename;
-    static_filename = [t1,'.bfc.nii.gz'];
-    opts.moving_mask = epimask_filename;   
-    opts.static_mask = [t1,'.mask.nii.gz'];
-    
-    opts.similarity = 'inversion';
-    opts.nthreads = str2num(config.epit1corr_numthreads);
-    register_files_affine(moving_filename, static_filename, output_filename, opts);
-    clear opts
-end
+% reorient_nifti_sform(epimask_filename,epimask_filename);
 %%  T1-based distortion correction
 fprintf(fp, 'Running T1-based distortion correction \n');
 outfile_corr = [fmri_b0,'.infile.corr.nii.gz'];
@@ -145,33 +83,28 @@ struct_output_filename = [fmri_b0,'.t1.corr.nii.gz'];
 
 opts.epi_mask = epimask_filename;
 opts.struct_mask = [t1,'.mask.nii.gz'];
-% opts.rigid_reg_mat = regmat;
 
 opts.num_threads = str2num(config.epit1corr_numthreads);
 fprintf(fp, '--number of threads: %d \n', opts.num_threads);
 
-% opts.non_uniformity_correction = false;
-% fprintf(fp, '--bias field correction: %s \n', mat2str(opts.non_uniformity_correction));
-% opts.similarity = 'inversion';
-
-% opts.rigid_similarity = 'inversion';
-% fprintf(fp, '--rigid_similarity: %s \n', opts.rigid_similarity);
-
-% opts.reg_res = 1.8;
-% fprintf(fp, '--reg_res: %s \n', opts.reg_res);
-
-% opts.debug = true;
+fprintf(fp, '--bias field correction: %s \n', mat2str(config.epit1corr_bias));
+if config.epit1corr_bias == '0'
+    opts.non_uniformity_correction = false;
+elseif config.epit1corr_bias == '1'
+    opts.non_uniformity_correction = true;
+end
 
 EPI_correct_files_registration_INVERSION(infileRAS, struct_filename, ...
     outfile_corr, struct_output_filename, opts);
 fprintf(fp, 'Successful! Corrected file is %s \n', outfile_corr);
 clear opts
 %%  remove reference volume
-outfile_final = [fmri,'.corr.nii.gz'];
+outfile_final = [fmri,'.epicorr.nii.gz'];
 epi_temp = load_untouch_nii_gz(outfile_corr);
 epi_corr = epi_temp;
 epi_corr.img(:,:,:,1) = [];
 epi_corr.hdr.dime.dim(5) =epi_corr.hdr.dime.dim(5)-1;
+sz = size(epi_corr.img);
 if sz(4)>1
     epi_corr.hdr.dime.dim(1) = 4;
 else
@@ -179,26 +112,4 @@ else
 end
 save_untouch_nii_gz(epi_corr,outfile_final);
 fprintf(fp, 'Final Output: %s', outfile_final);
-%% for testing: reregister to t1
-v = lung(outfile_corr);
-v.img(:,:,:,2:end) = [];
-v.hdr.dime.dim(1) = 3;
-v.hdr.dime.dim(5) = 1;
-outfile = [fmri_b0,'.infile.corr.refvol.nii.gz'];
-save_untouch_nii_gz(v,outfile)
-
-moving_filename = outfile;
-opts.moving_mask = epimask_filename;  
-static_filename = [t1,'.bfc.nii.gz'];
-output_filename = [fmri_b0,'.corr.func2t1.nii.gz'];
-opts.nthreads = str2num(config.epit1corr_numthreads);
-
-clear opts
-opts.similarity = 'inversion';
-register_files_affine(moving_filename, static_filename, output_filename, opts);
-
-transform_data_affine([t1,'.pvc.label.nii.gz'], 's', [fmri_b0,'.pvc.label.nii.gz'], ...
-    ref_filename, [t1,'.bfc.nii.gz'], [fmri_b0,'.corr.func2t1.rigid_registration_result.mat'], 'nearest');
-%%
-transform_data_affine(outfile, 'm', [fmri_b0,'.corr.func2t1.origreg.nii.gz'], ...
-      [fmri_b0,'.corr.ref.0_diffusion.nii.gz'], [t1,'.bfc.nii.gz'], regmat, 'spline');
+end

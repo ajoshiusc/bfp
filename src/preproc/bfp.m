22de86530ae990880471aa50dde642412b315982
sychoi <choisoyo@gmail.com>
2021-04-29 00:21:02 -0700

    fix issues with t1 extraction

--- src/preproc/bfp.m
+++ src/preproc/bfp.m
@@ -412,10 +412,10 @@ end
 fprintf('done\n');
 
 bsemask=fullfile(anatDir,sprintf('%s_T1w.mask.nii.gz',subid));
-% cmd=sprintf('fslmaths %s -thr 0 -bin -mul 255 %s -odt char',bsenew2,bsemask);
-% if ~exist(bsemask,'file')
-%     unix(cmd);
-% end
+cmd=sprintf('fslmaths %s -thr 0 -bin -mul 255 %s -odt char',bsenew2,bsemask);
+if ~exist(bsemask,'file')
+    unix(cmd);
+end
 
 if ~exist(bsemask, 'file')
     error(['Command: ', cmd, 'failed, existing.'])

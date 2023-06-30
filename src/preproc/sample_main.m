dd525bce355ce931f41d311798ee497adce65c1a
Anand A. Joshi <ajoshi@usc.edu>
2023-06-29 22:33:32 -0700

    update

--- src/preproc/sample_main.m
+++ src/preproc/sample_main.m
@@ -4,14 +4,14 @@ addpath(genpath('/home/ajoshi/projects/BrainSuite/bfp/src'));
 %    1050345 rest 2
 
 % Set the input arguments
-configfile='/home/ajoshi/sub-0009_ses-PIOP1/config_aaj.ini';
+configfile='/home/ajoshi/Downloads/bfp_sample/bfpconfig.ini';
 
-t1='/home/ajoshi/Downloads/for_david_debug_bfp/sub-0009/anat/sub-0009_T1w.nii.gz';
-fmri='/home/ajoshi/Downloads/for_david_debug_bfp/sub-0009/func/sub-0009_task-restingstate_acq-mb3_bold.nii.gz';
+t1='/home/ajoshi/Downloads/bfp_sample/input/sub08001/anat/mprage_anonymized.nii.gz';
+fmri='/home/ajoshi/Downloads/bfp_sample/input/sub08001/func/rest.nii.gz';
 
 %fmri='/home/ajoshi/Downloads/BFP_issues/ACTL005/ACTL005.BOLD.resting.nii.gz';
-studydir='/home/ajoshi/Downloads/for_david_debug_bfp/mybfpout/';
-subid='sub-0009';
+studydir='/home/ajoshi/Downloads/bfp_sample/output';
+subid='sub08001';
 sessionid='task-restingstate';
 TR='';
  

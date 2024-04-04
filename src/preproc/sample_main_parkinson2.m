25c1bcd1fc6125af2baef3f0ea97f8f5b6ff219e
Anand A. Joshi <ajoshi@usc.edu>
2024-04-04 13:34:46 -0700

    added parkinson analysis files

---
+++ src/preproc/sample_main_parkinson2.m
@@ -0,0 +1,32 @@
+clc;clear all;close all;restoredefaultpath;
+%addpath(genpath('/big_disk/ajoshi/coding_ground/bfp/supp_data'))
+addpath(genpath('/home/ajoshi/Projects/bfp/src'));
+%    1050345 rest 2
+
+% Set the input arguments
+configfile='/home/ajoshi/Projects/bfp/supp_data/config_bfp_preproc_minimal.ini';
+
+
+sublist = dir('/deneb_disk/parkinsons_fmri/neurocon/sub*');
+
+for j=1:length(sublist)
+
+    subid= sublist(j).name;
+
+    t1=['/deneb_disk/parkinsons_fmri/neurocon/',subid,'/anat/',subid,'_T1w.nii.gz'];
+    fmri=['/deneb_disk/parkinsons_fmri/neurocon/',subid,'/func/',subid,'_task-resting_run-1_bold.nii.gz'];
+
+    %fmri='/home/ajoshi/Downloads/BFP_issues/ACTL005/ACTL005.BOLD.resting.nii.gz';
+    studydir='/deneb_disk/parkinsons_fmri/neurocon_bfp_out';
+    sessionid='task-resting';
+    TR='';
+
+    setenv('BrainSuiteMCR','/home/ajoshi/Software/MATLAB/MATLAB_Runtime/R2023a');
+    try
+        % Call the bfp function
+        bfp(configfile, t1, fmri, studydir, subid, sessionid,TR);
+    catch
+        fprintf('***** sub with error: %s\n ***** \n',subid);
+    end
+
+end
--- src/visualization/main_bfp_bipolar2gif.m
+++ src/visualization/main_bfp_bipolar2gif.m
@@ -1,14 +1,15 @@
 clear all;close all;clc;
 restoredefaultpath;
-addpath(genpath('/ImagePTE1/ajoshi/code_farm/bfp/src'));
-bfpdir='/ImagePTE1/ajoshi/code_farm/bfp';
+addpath(genpath('/home/ajoshi/Projects/bfp/src'));
+
+bfpdir='/home/ajoshi/Projects/bfp';
 %fmridatfile='/home/ajoshi/coding_ground/fMRIReg/src/task_diff_dyn.mat';
-fmridatfile='/ImagePTE1/ajoshi/for_abhijit/bfp_out/sub-01/func/sub-01_task-rest_bold.32k.GOrd.filt.mat';
+fmridatfile='/home/ajoshi/Downloads/for_parsa/bfpoutput/sub1/func/sub1_task-restingstate_bold.32k.GOrd.mat';
 
 %'/deneb_disk/studyforrest_bfp/sub-01/func/sub-01_ses-movie_task-movie_run-3_bold.32k.GOrd.filt.mat';
-outgiffile='for_abhijit1.gif';
-TR=2;
+outgiffile='for_parsa.gif';
+TR=1.3;
 
 addpath(genpath(fullfile(bfpdir,'src')));
 
-make_movie_fmri(bfpdir,fmridatfile,outgiffile,TR,60);
+make_movie_fmri(bfpdir,fmridatfile,outgiffile,TR,6);

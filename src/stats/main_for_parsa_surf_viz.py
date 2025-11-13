2351e3af942928a8828e15a371b487e08b706a03
Anand A. Joshi <ajoshi@usc.edu>
2025-11-13 10:35:08 -0800

    Refactor code for loading and processing MAT files; improve formatting and readability

--- src/stats/main_for_parsa_surf_viz.py
+++ src/stats/main_for_parsa_surf_viz.py
@@ -6,15 +6,15 @@ from scipy.io import loadmat
 
 # load mat file /home/ajoshi/Downloads/c.mat
 
-cmat = loadmat('/home/ajoshi/Downloads/c.mat')
+cmat = loadmat("/home/ajoshi/Downloads/c.mat")
 
-c=cmat['c']
+c = cmat["c"]
 
-lsurf = readdfs('/home/ajoshi/Projects/bfp/supp_data/bci32kleft.dfs')
+lsurf = readdfs("/home/ajoshi/Projects/bfp/supp_data/bci32kleft.dfs")
 
-c = c[:len(lsurf.vertices)]
+c = c[: len(lsurf.vertices)]
 
-l = type('', (), {})()  # create empty object
+l = type("", (), {})()  # create empty object
 
 l.vertices = lsurf.vertices.copy()
 l.faces = lsurf.faces.copy()
@@ -26,19 +26,37 @@ lsurf.attributes = mean_curvature(lsurf)
 # Apply sigmoid transformation to clamp values smoothly
 lsurf.attributes = 1 / (1 + np.exp(-lsurf.attributes * 50))
 
-lsurf=patch_color_attrib(lsurf, cmap='Greys',clim=[0,1.5])
-
-l = patch_color_attrib(l, cmap='jet', clim=[np.min(c), np.max(c)])
-
-lsurf.vColor[c.flatten()> 0.15,:] = l.vColor[c.flatten()>0.15,:]  # color vertices with curvature > 0.5 with c values 
-
-# smooth surface 
+lsurf = patch_color_attrib(lsurf, cmap="Greys", clim=[0, 1.5])
+# smooth surface
 lsurf = smooth_patch(lsurf, iterations=1000)
 
+writedfs("left_curv_orig.dfs", lsurf)
 
-view_patch_vtk(lsurf, azimuth=100, elevation=180, roll=90,
-               outfile='left1.png', show=1)
+l = patch_color_attrib(l, cmap="jet", clim=[np.min(c), np.max(c)])
 
-writedfs('left_curv.dfs', lsurf)
+lsurf.vColor[np.abs(c.flatten()) > 0.15, :] = l.vColor[
+    np.abs(c.flatten()) > 0.15, :
+]  # color vertices with curvature > 0.5 with c values
 
 
+view_patch_vtk(lsurf, azimuth=100, elevation=180, roll=90, outfile="left1.png", show=1)
+
+writedfs("left_curv.dfs", lsurf)
+
+# add the code for right hemisphere as well
+rsurf = readdfs("/home/ajoshi/Projects/bfp/supp_data/bci32kright.dfs")
+rsurf.attributes = mean_curvature(rsurf)
+rsurf.attributes = 1 / (1 + np.exp(-rsurf.attributes * 50))
+rsurf = smooth_patch(rsurf, iterations=1000)
+writedfs("right_curv_orig.dfs", rsurf)
+c = c[len(lsurf.vertices) :]
+r = type("", (), {})()  # create empty object
+r.vertices = rsurf.vertices.copy()
+r.faces = rsurf.faces.copy()
+r.attributes = c.flatten()
+r = patch_color_attrib(r, cmap="jet", clim=[np.min(c), np.max(c)])
+rsurf.vColor[np.abs(c.flatten()) > 0.15, :] = r.vColor[
+    np.abs(c.flatten()) > 0.15, :
+]  # color vertices with curvature > 0.5 with c values
+view_patch_vtk(rsurf, azimuth=100, elevation=180, roll=90, outfile="right1.png", show=1)
+writedfs("right_curv.dfs", rsurf)
